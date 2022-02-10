294187e394867a3be97febc5188b0052189d6761
Eric Chon <echon92@gmail.com>
2022-02-10 12:37:18 -0700

    final clean up, deleted space, commented out code, extra lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
+  namespace :api do 
     namespace :v1 do
       get '/merchants/find', to: 'searches#show'
       get '/items/find_all', to: 'searches#index'
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,9 +4,5 @@ FactoryBot.define do
     description { Faker::Movies::HarryPotter.quote}
     unit_price { Faker::Number.decimal(l_digits: 3, r_digits: 3)}
     merchant
-    # after(:build) do |item|
-    #   merchant = create(:merchant)
-    #   item.merchant_id = merchant.id
-    # end
   end
 end
